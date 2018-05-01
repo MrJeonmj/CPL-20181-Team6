0384dd9e0c2b6162ff5b701fa4b96b12aa34b2c4
KWON Junhyeon <toktok94@knu.ac.kr>
2018-05-02 03:21:56 +0900

    deleted unsupported `import`; added `GraphView` TODO: `build.gradle` version error

--- app/src/main/java/com/example/user/jolp_v0/Statistics.java
+++ app/src/main/java/com/example/user/jolp_v0/Statistics.java
@@ -3,11 +3,24 @@ package com.example.user.jolp_v0;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.*;
+import com.jjoe64.graphview.series.*;
+
 public class Statistics extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 }
