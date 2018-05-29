a62150f26400cff34d8ca34a216accd5879d62c1
JHKwon <henry.kwon.0408@gmail.com>
2018-05-29 20:35:49 +0900

    Settings changed

--- app/src/main/java/com/example/user/jolp_v0/User_Setting.java
+++ app/src/main/java/com/example/user/jolp_v0/User_Setting.java
@@ -3,95 +3,51 @@ package com.example.user.jolp_v0;
 import android.annotation.TargetApi;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
-public class User_Setting extends PreferenceActivity {
+public class User_Setting extends PreferenceActivity
+{
     static String num;
-    static SharedPreferences prefer;
+    static SharedPreferences sharedPref;
 
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener()
+    {
         @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
+        public boolean onPreferenceChange(Preference preference, Object value)
+        {
             String stringValue = value.toString();
 
-            if (preference instanceof ListPreference) {
+            if (preference instanceof ListPreference)
+            {
                 // For list preferences, look up the correct display value in
                 // the preference's 'entries' list.
                 ListPreference listPreference = (ListPreference) preference;
                 int index = listPreference.findIndexOfValue(stringValue);
 
                 // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-
-            } else {
+                preference.setSummary(index >= 0? listPreference.getEntries()[index]: null);
+            }
+            else
+            {
                 // For all other preferences, set the summary to the value's
                 // simple string representation.
                 preference.setSummary(stringValue);
             }
-            //num = prefer.getString("partnerphonevalue","");
+            //num = sharedPref.getString("partnerphonevalue","");
             //Log.d(num+"*******","kk");
+
             return true;
         }
     };
 
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class GeneralPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.setting_xml);
-            setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-
-
-            prefer = PreferenceManager
-                    .getDefaultSharedPreferences(getActivity());
-
-            SharedPreferences.Editor editor = prefer.edit();
-            editor.putString("idvalue","abc" ); //키값, 저장값
-            editor.putString("phonevalue","010");
-            editor.commit();
-
-            bindPreferenceSummaryToValue(findPreference("namevalue"));
-            bindPreferenceSummaryToValue(findPreference("idvalue"));
-
-
-
-            Toast.makeText(getActivity(), prefer.getString("phonevalue",""), Toast.LENGTH_SHORT).show();
-
-
-        }
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getFragmentManager()
-                .beginTransaction()
-                .replace(android.R.id.content,
-                        new GeneralPreferenceFragment()).commit();
-
-    }
-
-    private static void bindPreferenceSummaryToValue(Preference preference) {
+    private static void bindPreferenceSummaryToValue(Preference preference)
+    {
         // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
@@ -101,5 +57,48 @@ public class User_Setting extends PreferenceActivity {
                 PreferenceManager
                         .getDefaultSharedPreferences(preference.getContext())
                         .getString(preference.getKey(), ""));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new GeneralPreferenceFragment())
+                .commit();
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.setting_xml);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+
+            sharedPref = PreferenceManager
+                    .getDefaultSharedPreferences(getActivity());
+
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("idvalue", "abc"); //키값, 저장값
+            editor.putString("phonevalue", "010");
+            editor.apply();
+
+            bindPreferenceSummaryToValue(findPreference("namevalue"));
+            bindPreferenceSummaryToValue(findPreference("idvalue"));
+
+            Toast.makeText(getActivity(), sharedPref.getString("phonevalue", ""), Toast.LENGTH_SHORT).show();
+
+
+        }
     }
 }
